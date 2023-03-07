95ad6844478dd8a75dd611e01c3781e80c2a4fcb
hoandbh <project2023bh@gmail.com>
2023-03-07 20:21:42 +0200

    hadas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom"
 import Home from "./pages/Home";
 
 function App() {
-  return (
-   <Router>
-    <nav>
-      {/*why to '/' ?? */}
-      {/* <NavLink to="/">Koogle_ </NavLink> */}
-      <NavLink to="/home">Home </NavLink>
-    </nav>
-    <Routes>
-      {/* <Route path="/" element={<Home />}/> */}
-      <Route path='/home' element={<Home></Home>}></Route>
-    </Routes>
-   </Router>
-  );
+  return <>
+    <Home></Home>
+  </>
 }
 
 export default App;
--- src/pages/Coordinators/Questionnaire/New.jsx
+++ src/pages/Coordinators/Questionnaire/New.jsx
@@ -1,46 +1,86 @@
-import {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import React from 'react';
+// import {useState} from 'react';
+// import {Link, useNavigate} from 'react-router-dom';
+// import React from 'react';
 
 
-const New = () => {
+// const New = () => {
 
-    const createQuestionnaire = ()=>{
-      alert("new test created, term: "+ term +" date: "+date);
+//     const createQuestionnaire = ()=>{
+//       alert("new test created, term: "+ term +" date: "+date);
         
-    }
+//     }
 
-    const [date, setDate] = useState();
-    const  [term, setTerm] = useState('A');
+//     const [date, setDate] = useState();
+//     const  [term, setTerm] = useState('A');
 
-    //if the function does a few things we can also do it like this
-    // const dateChanger = event =>{
-    //   setDate(event.target.value);
-    //   console.log(this.date);
-    // }
-    //   const navigate = useNavigate();
-
-
-  return <>
-    <br/>
-    <p>בחרי תאריך למבחן</p><br/><br/>
-    <input  type={'date'}  onBlur = {e=>{setDate(e.target.value)}}></input><br/><br/>
-
-    <select name='terms' id='term-select' onBlur={e=>setTerm(e.target.value)}>
-      <option value={''}> מועד המבחן</option>
-      <option value={'A'}>מועד א</option>
-      <option value={'B'}>מועד ב</option>
-      <option value={'C'}>מועד ג</option>
-      <option value={'D'}>מועד ד</option>
-      <option value={'E'}>מועד מיוחד</option>
-
-    </select><br/> <br/>
-
-    <button onClick={createQuestionnaire}>create questionnaire</button>
+//     //if the function does a few things we can also do it like this
+//     // const dateChanger = event =>{
+//     //   setDate(event.target.value);
+//     //   console.log(this.date);
+//     // }
+//     //   const navigate = useNavigate();
 
 
-  </>
+//   return <>
+//     <br/>
+//     <p>בחרי תאריך למבחן</p><br/><br/>
+//     <input  type={'date'}  onBlur = {e=>{setDate(e.target.value)}}></input><br/><br/>
+
+//     <select name='terms' id='term-select' onBlur={e=>setTerm(e.target.value)}>
+//       <option value={''}> מועד המבחן</option>
+//       <option value={'A'}>מועד א</option>
+//       <option value={'B'}>מועד ב</option>
+//       <option value={'C'}>מועד ג</option>
+//       <option value={'D'}>מועד ד</option>
+//       <option value={'E'}>מועד מיוחד</option>
+
+//     </select><br/> <br/>
+
+//     <button onClick={createQuestionnaire}>create questionnaire</button>
+
+
+//   </>
+// }
+
+
+
+
+import React, { useRef } from 'react';
+//bracha, each time useState change- the component re-render. if you don't want it- use useRef
+
+function New() {
+  const dateRef = useRef('');
+  const termRef = useRef('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formData = {
+      date: dateRef.current.value,
+      term: termRef.current.value
+    };
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        date:
+        <input type="date" ref={dateRef} required />
+      </label>
+      <label>
+        term:
+        <select ref={termRef} required >
+          <option> מועד המבחן</option>
+          <option value={'A'}>מועד א</option>
+          <option value={'B'}>מועד ב</option>
+          <option value={'C'}>מועד ג</option>
+          <option value={'D'}>מועד ד</option>
+          <option value={'E'}>מועד מיוחד</option>
+        </select>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default New;
-
