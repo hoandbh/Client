aee3c610b630f102cb17b4117a677ef00e74f853
Hadas-Lab <36214128472@mby.co.il>
2023-03-09 22:32:24 +0200

    single view

--- src/pages/Coordinators/Questionnaire/Single.jsx
+++ src/pages/Coordinators/Questionnaire/Single.jsx
@@ -4,16 +4,21 @@ import Axios from "axios";
 
 const Single = () => {
 
-  const [data, setData] = useState([{}]);
+  const [data, setData] = useState({});
+  const [partsNum, setPartsNum] = useState(0);
+
+  //
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const id = searchParams.get('id');
 
   useEffect( () => { 
     const fetchData = async () => { 
-      const {data:questionnaireInfo} = await Axios.get(`http://localhost:3600/api/questionnaire/full/${id}`)
-      setData(questionnaireInfo);
-      
+      const {data} = await Axios.get(`http://localhost:3600/api/questionnaire/full/${id}`)
+      const questionnaire = data[0]
+      setData(questionnaire);
+      setPartsNum(questionnaire.parts_in_questionnaire.length);
+      console.log(questionnaire)
     }
     fetchData();
   },[])
@@ -33,9 +38,29 @@ const Single = () => {
   //}
   //</ul>
 
+  const viewQst = (qst) => {
+    return <p>{qst.content}</p>
+  }
+
+  const viewPart = (part) => {
+    return <>
+      <p>headline: {part.headline}</p>
+      <p>number: {part.number_in_questionnaire}</p>
+      {part.questions_in_part && <ul>
+        {part.questions_in_part.map((qst) => <li>{viewQst(qst)}</li>)}
+      </ul>}
+    </>
+  }
+
+
   return <>
-    
     <h1>single component {id}</h1>
+    {data && <h4>date: {data.date}</h4>}
+    {data && <h4>owner: {data.owner}</h4>}
+    {data && data.parts_in_questionnaire && <ul>
+      {data.parts_in_questionnaire.map((part) => <li>{viewPart(part)}</li>)}
+    </ul>}
+    <button>add part</button>
   </>
 }
 
