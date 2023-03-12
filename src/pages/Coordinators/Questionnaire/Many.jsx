250551b22bed8f48d61b0d825cf0ef387a09c029
hoandbh <project2023bh@gmail.com>
2023-03-12 19:57:55 +0200

    hi

--- src/pages/Coordinators/Questionnaire/Many.jsx
+++ src/pages/Coordinators/Questionnaire/Many.jsx
@@ -1,25 +1,54 @@
-import {useState } from "react";
+import { useEffect, useState } from "react";
 import Axios from "axios";
 
+const showQstnrDetails = (props) => {
+
+    // const qstnr = props.qstnr;
+    console.log(props)
+    return <>
+        <p>id: {props.id_questionnaire}</p>
+        <p>owner: {props.owner}</p>
+        <p>date: {props.date}</p>
+        <p>term: {props.term}</p>
+
+    </>
+}
 
 const Many = () => {
 
     const [qst, setQst] = useState([]);
+    // const [qsntrByOwner, setQstnrByOwner] = useState([]);
 
-    const showQuestionnaires = async event => {
-        const {data:d} = await Axios.get('http://localhost:3600/api/questionnaire');
-        const qstInfo = d.map(qst => qst.date);
+    const showQuestionnaires = async () => {
+        const { data: d } = await Axios.get('http://localhost:3600/api/questionnaire/owner/131');
+        const qstInfo = d.map(qst => showQstnrDetails(qst));
+        setQst(qstInfo);
+    }
+    const showAllQuestionnaires = async () => {
+        const { data: d } = await Axios.get('http://localhost:3600/api/questionnaire');
+        // console.log(d);
+        const qstInfo = d.map(qst => showQstnrDetails(qst));
         setQst(qstInfo);
     }
 
+
+    useEffect(() => {
+        showQuestionnaires();
+    }, [])
+
     return <>
         many component
-        <button onClick={showQuestionnaires}>See Test</button><br /><br />
-        {qst && 
+
+        <button onClick={showAllQuestionnaires}>See All</button><br /><br />
+        {/* {qst &&
             <ul>
                 {qst.map(q => <li>{q}</li>)}
             </ul>
-        }
+        } */}
+        {qst &&
+            <ul>
+                {qst.map(q => <li>{q}</li>)}
+            </ul>}
     </>
 }
 
