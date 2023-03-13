cf04b2fb2108f9ca7e25ee9ca37fd785cebaad62
Hadas-Lab <36214128472@mby.co.il>
2023-03-13 12:05:39 +0200

    .

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom"
 import Single from './pages/Coordinators/Questionnaire/Single';
 import New from './pages/Coordinators/Questionnaire/New';
 import Many from "./pages/Coordinators/Questionnaire/Many"
-import { Button, Box, Tabs, Tab } from "@mui/material";
 
 function App() {
   return <>
     <Router>
       <nav>
-        <NavLink to="/"><Tab value = "one" label="הוספת מבחן" href="./Questionnaire/new" />   </NavLink><span>----</span>
+        <NavLink to="/">Home  </NavLink><span>----</span>
         <NavLink to="/questionnaire/new">new questionnaire</NavLink><span>----</span>
         <NavLink to="/questionnaire/many">many</NavLink>
       </nav>
