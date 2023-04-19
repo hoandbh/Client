eb8894432202734075f3a7f1b12bdb286e85e16a
hoandbh <project2023bh@gmail.com>
2023-04-19 15:16:41 +0300

    d

--- src/pages/Coordinators/Questionnaire/mix.jsx
+++ src/pages/Coordinators/Questionnaire/mix.jsx
@@ -1,14 +1,18 @@
 import { useNavigate } from 'react-router-dom';
-import { React , useRef, useState, useContext } from 'react';
+import { React, useRef, useState, useContext,useEffect } from 'react';
 import { AuthContext } from '../../../context/authContext'
+import Box from '@mui/material/Box';
 
-const mixQuestionnaire = () =>{
+const mixQuestionnaire = () => {
 
-
-    const navigate = useNavigate();
 
     return <>
-    
+
+        <Box>
+            <p>HI</p>
+        </Box>
+
+
     </>
 }
 
