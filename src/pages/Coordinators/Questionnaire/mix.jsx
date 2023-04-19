bcbf8876d89d823091c6d5c7ed9e7a2479a9c5f8
hoandbh <project2023bh@gmail.com>
2023-04-19 14:59:20 +0300

    start the mix componenet

---
+++ src/pages/Coordinators/Questionnaire/mix.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import { React , useRef, useState, useContext } from 'react';
+import { AuthContext } from '../../../context/authContext'
+
+const mixQuestionnaire = () =>{
+
+
+    const navigate = useNavigate();
+
+    return <>
+    
+    </>
+}
+
+
+export default mixQuestionnaire;
