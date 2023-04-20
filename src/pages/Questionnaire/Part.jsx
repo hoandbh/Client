73cd876e0be4121042fef6a8ba1306ef89ce2b5d
Hadas-lab <36214128472@mby.co.il>
2023-04-20 20:34:51 +0300

    part-desinger

--- src/pages/Questionnaire/Part.jsx
+++ src/pages/Questionnaire/Part.jsx
@@ -4,20 +4,15 @@ import { Button, List, ListItem, Divider, Typography } from '@mui/material';
 import QuestionCard from "./Question/Card";
 import QuestionCardTry from "./Question/CardTry";
 import QuestionForm from './Question/Form';
+import QuestionFormTry from './Question/FormTry';
 
 const Part = ({part}) => {
 
   const [questions, setQuestions] = useState(part.questions_in_part);
   const [open, setOpen] = useState(false);
-  const [questionContent, setQuestionContent] = useState('');
-  const [correctAnswer, setCorrectAnswer] = useState('');
-  const [incorrectAnswers, setIncorrectAnswers] = useState([]);
-
-  //const apiUrl = process.env.REACT_APP_API_URL;
 
   const handleDeleteQuestion = () => {
     fetchQuestions();
-    //render;
   }
 
   const fetchQuestions = async () => {
@@ -25,77 +20,46 @@ const Part = ({part}) => {
     setQuestions(data);
   }
 
-  const postQuestionAndAnsawers = async () => {
+  const postQuestionAndAnsawers = async (values) => {
     const { data } = await axios.post(`http://localhost:3600/api/question`,
-        {
-          content : questionContent,
-          part_id : part.id
-        }
-      )
-      const qstId = data.id;
-      await axios.post(`http://localhost:3600/api/answer`,
       {
-        content: correctAnswer,
-        is_correct: true,
-        question_id: qstId
+        content : values.questionContent,
+        part_id : part.id
       }
+    )
+    const qstId = data.id;
+    await axios.post(`http://localhost:3600/api/answer`,
+    {
+      content: values.correctAnswer,
+      is_correct: true,
+      question_id: qstId
+    } 
+    );
+    for (const answer of values.incorrectAnswers) {
+      await axios.post(`http://localhost:3600/api/answer`,
+        {
+          content: answer.content,
+          is_correct: false,
+          question_id: qstId
+        }
       );
-      for (const ans of incorrectAnswers) {
-        await axios.post(`http://localhost:3600/api/answer`,
-          {
-            content: ans,
-            is_correct: false,
-            question_id: qstId
-          }
-        );
-      }
-      fetchQuestions();
+    }
+    fetchQuestions();
   }
-  
+
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  const handleSave = async () => {
-    await postQuestionAndAnsawers()
-    setIncorrectAnswers([]);
+  const handleSave = async (values) => {
+    await postQuestionAndAnsawers(values)
     setOpen(false);
   };
 
   const handleCancel = () => {
     setOpen(false);
   };
-
-  const handleQuestionChange = (event) => {
-    setQuestionContent(event.target.value);
-  };
-
-  const handleCorrectAnswerChange = (event) => {
-    setCorrectAnswer(event.target.value);
-  };
-
-  const handleIncorrectAnswerChange = (index, event) => {
-    const value = event.currentTarget.value;
-    const answers = [...incorrectAnswers];
-    answers[index] = value;
-    setIncorrectAnswers(answers);
-  };
-
-  useEffect(() => {
-  },[incorrectAnswers])
-
-  const addIncorrectAnswerField = () => {
-    setIncorrectAnswers(prevAnswers => [...prevAnswers, '']);
-  }
-
-  const removeIncorrectAnswerField = (index) => {
-    // const answers = [...incorrectAnswers]
-    // answers.splice(index, 1);
-    // setIncorrectAnswers(answers);
-    setIncorrectAnswers(prevAnswers => prevAnswers.filter((answer, i) => i !== index));
-  }
   
-
   const initialValues = {
     question: '',
     correctAnswer: '',
@@ -103,20 +67,16 @@ const Part = ({part}) => {
   };
   
   return <>
-    <Divider sx={{ mt: 2, mb: 2 }} />
-    <Typography sx={{ typography: 'subtitle1' ,fontWeight: 'light', m: 1}}>
-      {part.headline}
-    </Typography>
-    <Typography sx={{ typography: 'subtitle2' ,fontWeight: 'light', m: 1}}>
-      serial number: {part.serial_number}
-    </Typography>
+    <Divider textAlign="left" sx={{ mt: 2, mb: 2 }} >
+      <Typography sx={{ typography: 'subtitle1' ,fontWeight: 'light', m: 1}}>
+        {`${part.headline}(${part.serial_number})`}
+      </Typography>
+    </Divider>
 
     {questions && questions.length > 0 &&
       <>
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
           {questions.map(qst => {
-            //initialValues={{ questionContent:'questionContent', correctAnswer: 'correctAnswer', incorrectAnswers: [] }}
-            // return <><ListItem alignItems="flex-start" key={qst.id}><QuestionCard question={qst} onDelete={handleDeleteQuestion}/></ListItem></>
             return <>
               <ListItem alignItems="flex-start" key={qst.id}>
                 <QuestionCardTry 
@@ -133,22 +93,14 @@ const Part = ({part}) => {
     <Button variant="contained" color="primary" onClick={handleClickOpen}>
       add question to part {part.serial_number}
     </Button>
-      <QuestionForm 
-        options={{setCorrectAnswer,
-                  setQuestionContent,
-                  initialValues,
-                  open,
-                  handleQuestionChange,
-                  handleCorrectAnswerChange,
-                  incorrectAnswers,
-                  handleIncorrectAnswerChange,
-                  addIncorrectAnswerField,
-                  removeIncorrectAnswerField
-                  ,handleCancel,
-                  handleSave,
-                  questionContent,
-                  correctAnswer}}
-      />
+      {open && <QuestionFormTry 
+        options={{
+          open,
+          initialValues,
+          handleCancel,
+          handleSave,
+        }}
+      />}
   </>
 }
 
