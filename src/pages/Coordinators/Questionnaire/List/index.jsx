3c78f58d919a75f58746fb64e6db6380aae0f0a4
hoandbh <project2023bh@gmail.com>
2023-04-20 18:48:44 +0300

    buttons on questionnaire card

--- src/pages/Coordinators/Questionnaire/List/index.jsx
+++ src/pages/Coordinators/Questionnaire/List/index.jsx
@@ -1,59 +1,76 @@
 import { useEffect, useState, useContext } from "react";
 import axios from "axios";
-import { ListItem, List, Paper} from '@mui/material';
+import { ListItem, List, Paper, Fab } from '@mui/material';
 import { AuthContext } from '../../../../context/authContext'
 import LinkIcon from '@mui/icons-material/Link';
 import { useNavigate } from "react-router-dom";
 import IconButton from '@mui/material/IconButton';
-
-const QuestionnaireCard = ({questionnaire}) => {
+import EditIcon from '@mui/icons-material/Edit';
+const QuestionnaireCard = ({ questionnaire }) => {
 
   const navigate = useNavigate();
 
-  
+
   const openEdit = () => {
-    // navigate(`/questionnaire/${questionnaire.id}`);
-    navigate(`/questionnaire/complete/${questionnaire.id}`); 
+    navigate(`/questionnaire/${questionnaire.id}`);
   }
 
+  const openFile = () => {
+    navigate(`/questionnaire/complete/${questionnaire.id}`);
+
+  }
   useEffect(() => {
     console.log(questionnaire);
-  },[])
+  }, [])
 
   return <>
     <Paper
-    sx={{
-    p: 2,
-    maxWidth: 500,
-    flexGrow: 1,
-    fontFamily: 'Open Sans, sans-serif',
-    backgroundColor: (theme) =>
-        theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    }}
-    > 
-      { 
+      sx={{
+        p: 2,
+        maxWidth: 1000,//500,
+        flexGrow: 1,
+        fontFamily: 'Open Sans, sans-serif',
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+      }}
+    >
+      {
         `Questionnaire number: ${questionnaire.id}.
         Teacher: ${questionnaire.owner}.
         Date: ${new Date(questionnaire.date).toLocaleDateString()}.
         `
       }
-
-      <IconButton onClick={openEdit}>
-        <LinkIcon/>
+      {/* 
+      <IconButton onClick={openFile}>
+        <LinkIcon/>Open Test File
       </IconButton>
+      <IconButton onClick={openEdit}>
+        <EditIcon/> Edit Test 
+      </IconButton> */}
+
+      {/* <div style="float:left;"> */}
+      <Fab variant="extended" onClick={openEdit}>
+        <EditIcon /> Edit Test
+      </Fab>
+      <Fab variant="extended" onClick={openFile}>
+        <LinkIcon />Open Test File
+      </Fab>
+
+
+      {/* </div> */}
     </Paper>
   </>
 }
 
 const QuestionnairesList = () => {
 
-  const {currentUser} = useContext(AuthContext);
-  const [currentUserId, setCurrentUserId] = useState(currentUser?.id);  
+  const { currentUser } = useContext(AuthContext);
+  const [currentUserId, setCurrentUserId] = useState(currentUser?.id);
   const [questionnaires, setQuestionnaires] = useState([]);
 
   useEffect(() => {
     setCurrentUserId(currentUser?.id);
-  },[currentUser])
+  }, [currentUser])
 
   const fetchData = async () => {
     const { data } = await axios.get(`http://localhost:3600/api/questionnaire?owner=${currentUserId}`);
@@ -64,16 +81,16 @@ const QuestionnairesList = () => {
     fetchData();
   }, [])
 
-    return <>
-        {
-          questionnaires &&
-          <List>
-                {questionnaires.map(q => <ListItem >
-                  <QuestionnaireCard questionnaire={q}/>
-                </ListItem>)}
-          </List>
-        }
-    </>
+  return <>
+    {
+      questionnaires &&
+      <List>
+        {questionnaires.map(q => <ListItem >
+          <QuestionnaireCard questionnaire={q} />
+        </ListItem>)}
+      </List>
+    }
+  </>
 }
 
 export default QuestionnairesList;
