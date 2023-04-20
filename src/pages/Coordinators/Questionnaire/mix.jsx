10af222bb678a60d0d59974b06b837bf6e54e6cb
hoandbh <project2023bh@gmail.com>
2023-04-20 15:13:02 +0300

    added components to mix page

--- src/pages/Coordinators/Questionnaire/mix.jsx
+++ src/pages/Coordinators/Questionnaire/mix.jsx
@@ -4,6 +4,10 @@ import { AuthContext } from '../../../context/authContext'
 import Box from '@mui/material/Box';
 import { useState } from 'react';
 import axios from 'axios';
+import { Card, Fab, Paper, TextField } from '@mui/material';
+import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
+import { spacing } from '@mui/system';
+
 
 const MixQuestionnaire = () => {
 
@@ -13,8 +17,8 @@ const MixQuestionnaire = () => {
     const [questionnaireDetails, setQuestionnaireDetails] = useState({});
 
 
-    const fetchData = async()=>{
-        const { data: questionnaire} = await axios.get(`http://localhost:3600/api/questionnaire/${id}`);
+    const fetchData = async () => {
+        const { data: questionnaire } = await axios.get(`http://localhost:3600/api/questionnaire/${id}`);
         setQuestionnaireDetails(questionnaire);
         console.log(questionnaire)
 
@@ -22,13 +26,38 @@ const MixQuestionnaire = () => {
 
     useEffect(() => {
         fetchData();
-      }, [])
+    }, [])
 
     return <>
 
-        <Box>
-            <h1>hi</h1>
-        </Box>
+        <Paper sx={{ '& > :not(style)': { m: 'auto' } }}>
+
+            <h1> Test Mixing </h1><br />
+            <Fab variant="extended">
+                <ContentPasteSearchIcon sx={{ mr: 1 }} />
+                View File Of Master Questionnaire
+            </Fab>
+
+        </Paper>
+
+        <Card>
+            <h3>Finish And Mix
+            </h3>
+            <TextField
+          id="outlined-number"
+          label="Number of Versions"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <br/><br/>
+        <Fab variant="extended" color='primary'>
+            Complete And Create Versions
+            </Fab>
+        </Card>
+
+
 
 
     </>
