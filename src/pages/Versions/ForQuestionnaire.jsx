9cf5f14bcb5b727f9118791b61b06af59a8a790c
hoandbh <project2023bh@gmail.com>
2023-04-26 20:58:00 +0300

    show links on page

--- src/pages/Versions/ForQuestionnaire.jsx
+++ src/pages/Versions/ForQuestionnaire.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-
+import { Chip, List, ListItem, ListItemButton } from '@mui/material';
 
 const VersionsPdf = () => {
 
     const { Qid } = useParams();
+    const [links, setLinks] = useState([]);
 
+    useEffect(() => {
+        fetchVersions();
+    }, [])
+
+    const fetchVersions = async() => {
+        const {data: d} = await axios.get(`http://localhost:3600/api/downloads/versions/2`);
+        console.log(d);
+        setLinks(d);
+    }
+    const designLinks = (arr)=>{
+        arr.forEach(element => {
+            <Chip label="Chip Filled" />
+
+        });
+
+    }
     return (
-        <div> Hellow Hello {Qid}</div>
+        <>
+        <div> Files Paths {Qid}</div>
+        {links &&
+        <List>
+            {links.map(l =><ListItemButton>{l}</ListItemButton>)}
+        </List>
+        }
+        </>
     )
 }
 
