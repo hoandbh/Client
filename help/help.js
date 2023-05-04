8347248c640185ff04157cfe6629257014381bf1
Hadas-lab <36214128472@mby.co.il>
2023-05-04 22:18:39 +0300

    add image button

--- help/help.js
+++ help/help.js
@@ -1,129 +1,36 @@
+import { useState } from "react"
+import axios from "axios"
+import { Button } from "@mui/material";
 
-import { Button, Paper, DialogTitle, DialogContent, DialogActions, TextField, Divider, IconButton } from '@mui/material';
-import { Formik, Field, FieldArray } from 'formik';
-import DeleteIcon from '@mui/icons-material/Delete';
+const Uploader = ({ file, setFile, label }) => {
 
-const QuestionForm = ({ options }) => {
+  const [image, setImage] = useState('');
 
-  const handleSaveFormik = (values) => {
-  if (options.isEditing) {
-    options.handleEdit(values);
-  } else {
-    options.handleSave(values);
+  const handleAdd = async () => {
+    console.log(image);
+    try {
+      const picture = await axios.post(`http://localhost:3600/api/question/1/image`, image, { headers: { "Content-Type": "multipart/form-data" } })
+      console.log(picture);
+      const r = await picture.json();
+      console.log(r);
+      // const picture = await axios.post(`http://localhost:3600/images`, image, { headers: { 'Authorization': 'Bearer ' + token, "Content-Type": "multipart/form-data" } })
+    }
+    catch (err){
+      console.log(err);
+    }
   }
-  };
 
-  const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  textField: {
-    display: 'inline-block',
-  },
-  iconButton: {
-    display: 'inline-block',
-  },
-  };
+  const onSelectFile = (e) => {
+    setImage(e.target.files[0])
+  }
 
   return (
-  <Formik initialValues={options.initialValues} >
-    {formik => (
-    <form>
-      <Paper
-      sx={{
-        p: 2,
-        margin: 'auto',
-        maxWidth: 500,
-        flexGrow: 1,
-        backgroundColor: (theme) =>
-        theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-      }}
-      open={options.open}
-      >
-        <DialogTitle>{options.isEditing ? 'Edit question' : 'New question'}</DialogTitle>
-        <DialogContent>
-        {/* question */}
-        <Field
-          required
-          as={TextField}
-          margin="dense"
-          label="question"
-          type="text"
-          fullWidth
-          value={formik.values.questionContent}
-          name="questionContent"
-        // onFocus={event => {
-        //   const input = event.target;
-        //   input.setSelectionRange(0, input.value.length);
-        // }}
-        />
-        {/* correct answer */}
-        <Divider sx={{ mt: 2, mb: 2 }} />
-        <Field
-          as={TextField}
-          margin="dense"
-          label="correct answer"
-          type="text"
-          fullWidth
-          value={formik.values.correctAnswer}
-          name="correctAnswer"
-        // onFocus={event => {
-        //   const input = event.target;
-        //   input.setSelectionRange(0, input.value.length);
-        // }}  
-        />
-        <Divider sx={{ mt: 2, mb: 2 }} />
-        {/* incorrect answers */}
-        <FieldArray name="incorrectAnswers">
-          {({ push, remove }) => (
-          <>
-            {formik.values.incorrectAnswers?.map((answer, index) => (
-            <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
-              <Field
-              style={styles.textField}
-              as={TextField}
-              margin="dense"
-              label={`incorrect answer ${index + 1}`}
-              type="text"
-              fullWidth
-              name={`incorrectAnswers[${index}].content`}
-              value={formik.values.incorrectAnswers[index].content}
-              // onFocus={event => {
-              //   const input = event.target;
-              //   input.setSelectionRange(0, input.value.length);
-              // }}
-              />
-              <IconButton
-              aria-label="delete"
-              onClick={() => remove(index)}
-              >
-              <DeleteIcon />
-              </IconButton>
-            </div>
-            ))}
-            <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => push('')}
-            //onClick={() => push({content:`Write the incorrect answer number ${formik.values.incorrectAnswers.length + 1} here...`})}
-            sx={{ mt: 2 }}
-            >
-            Add incorrect answer
-            </Button>
-          </>
-          )}
-        </FieldArray>
-        </DialogContent>
-        <DialogActions>
-        <Button onClick={options.handleCancel}>Cancel</Button>
-        <Button onClick={() => handleSaveFormik(formik.values)}>{options.isEditing ? 'Save' : 'Add'}</Button>
-        </DialogActions>
-      </Paper>
-    </form>
-    )}
-  </Formik>
-  );
-}
+    <>
+      <label htmlFor="file"> {label ? label : "File"} </label>
+      <input accept="image/*" type="file" onChange={onSelectFile} name="file" />
+      <Button onClick={handleAdd}>add</Button>
+    </>
+  )
+};
 
-export default QuestionForm;
+export default Uploader
--- src/components/Uploader.jsx
+++ src/components/Uploader.jsx
@@ -1,36 +1,91 @@
-import { useState } from "react"
-import axios from "axios"
-import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, CircularProgress } from "@mui/material";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
-const Uploader = ({ file, setFile, label }) => {
+const Uploader = ({ setFile }) => {
 
-  const [image, setImage] = useState('');
+  const [selectImage, setSelectImage] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const questionId = 1;
+  // need to get the question id, but if it's new question??
+  // what about the setFile?
 
-  const handleAdd = async () => {
-    console.log(image);
-    try {
-      const picture = await axios.post(`http://localhost:3600/api/question/1/image`, image, { headers: { "Content-Type": "multipart/form-data" } })
-      console.log(picture);
-      const r = await picture.json();
-      console.log(r);
-      // const picture = await axios.post(`http://localhost:3600/images`, image, { headers: { 'Authorization': 'Bearer ' + token, "Content-Type": "multipart/form-data" } })
+  const onSelectImage = (e) => {
+    setSelectImage(e.target.files[0]);
+  };
+
+  useEffect(()=>{
+    handleUpload();
+  },[selectImage])
+
+  const handleUpload = () => {
+    console.log('selectImage')
+    console.log(selectImage)
+    setError(null);
+    if (selectImage) {
+      setIsLoading(true);
+      const formData = new FormData();
+      formData.append("file", selectImage);
+      axios
+        .post(`http://localhost:3600/api/question/${questionId}/image`, formData)
+        .then(({ data }) => {
+          if (data?.name) {
+            setFile(data.name);
+          }
+        })
+        .catch((err) => {
+          console.error("Error uploading image", err);
+          setError("Error uploading image. Please try again.");
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } else {
+      setError("Please select an image to upload.");
     }
-    catch (err){
-      console.log(err);
-    }
-  }
-
-  const onSelectFile = (e) => {
-    setImage(e.target.files[0])
-  }
+  };
 
   return (
     <>
-      <label htmlFor="file"> {label ? label : "File"} </label>
-      <input accept="image/*" type="file" onChange={onSelectFile} name="file" />
-      <Button onClick={handleAdd}>add</Button>
+      <div style={{ display: "inline-block", position: "relative" }}>
+        {isLoading ? (
+          <CircularProgress size={24} />
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddPhotoAlternateIcon />}
+            onClick={() => document.getElementById("image-input").click()}
+            disabled={isLoading}
+            aria-label="Add Image"
+          >
+            Add Image
+          </Button>
+        )}
+        <input
+          id="image-input"
+          type="file"
+          accept="image/*"
+          name="file"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            opacity: 0,
+            width: "100%",
+            height: "100%",
+            cursor: "pointer",
+          }}
+          onChange={onSelectImage}
+        />
+      </div>
+      {error && (
+        <p style={{ color: "red", marginTop: "10px" }}>{error}</p>
+      )}
     </>
-  )
+  );
 };
 
-export default Uploader
+export default Uploader;
